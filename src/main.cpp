84ee5a21a26c0bb1d355ccad4388b341ae7107d2
Ilia <fortyhell@gmail.com>
2020-06-05 00:18:49 +0300

    Add game process in main()

--- src/main.cpp
+++ src/main.cpp
@@ -11,30 +11,22 @@ class Map {
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
                 map[i][j] = field[i + 1][(j + 1) * 2];
-                //cout << map[i][j];
             }
-            //cout << '\n';
         }
         for (int i = 0; i < 3; ++i) {//перебор строк/столбцов
             //rows
             if (map[i][0] == map[i][1] && map[i][1] == map[i][2]) {
-                //cout << map[i][j] << '.' << map[i][j + 1] << '.';
-                //cout << map[i][j + 2] << '\n';
-                //cout << i << '.' << j << ",1: " << map[i][j];
                 return map[i][0];
             }
             // columns
             if (map[0][i] == map[1][i] && map[1][i] == map[2][i]){
-                //cout << "2: " << map[j][i];
                 return map[0][i];
             }
         }
         if (map[0][0] == map[1][1] && map[1][1] == map[2][2]) {
-            //cout << "3: " << map[1][1];
             return map[1][1];
         }
         if (map[0][2] == map[1][1] && map[1][1] == map[2][0]) {
-            //cout << "4: " << map[1][1];
             return map[1][1];
         }
         return ' ';
@@ -52,6 +44,9 @@ public:
         }
     }
     bool fill_point(int x, int y, char value) {
+        if ((x > 2 || x < 0) && (y > 2 || y < 0)) {
+            return false;
+        }
         x++;
         y = (y + 1) * 2;
         if (field[x][y] != ' '){
@@ -60,21 +55,42 @@ public:
         field[x][y] = value;
         return true;
     }
-    void win() {
+    bool win() {
         char w = check_win();
         if (w != ' ') {
             cout << '\n' << w << " is win!\n";
+            return true;
         }
+        return false;
     }
 };
 
 int main() {
     Map map;
-    map.fill_point(0, 1, 'X');
-    map.fill_point(0, 2, 'X');
-    map.fill_point(0, 0, 'X');
-    map.fill_point(1, 2, '0');
+    int x, y;
+    char s, player = 'X';
+    cout << "Enter coordinates as 1,2. For stop enter -1\n";
     map.print_field();
-    map.win();
+    do {
+        s = '.';// disable infinity loop when tap ctrl+d
+        cout << player << " move: ";
+        cin >> x >> s >> y;
+        if (x == -1) {
+            break;
+        }
+        if (map.fill_point(x, y, player)) {
+            map.print_field();
+            if (map.win()) {
+                break;
+            }
+            if (player == 'X') {
+                player = '0';
+            } else { // player == '0'
+                player = 'X';
+            }
+        } else {
+            cout << "Incorrect. Try again\n";
+        }
+    } while(s == ',');
     return 0;
 }
